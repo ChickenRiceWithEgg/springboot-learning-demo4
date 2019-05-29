46be5a7a99776c9717e3db6ef9b1b5d561dc88d8
ChickenRiceWithEgg <jingwei144@foxmail.com>
2019-05-30 00:36:40 +0800

    学习前台向后台传递参数  2.0 @RequestBody  传递JOSN类型数据 ServletInputStream sis = new HttpServletRequest.getInputStream()  来获取http请求传入的数据

--- src/main/java/com/xjw/demo4/controller/HelloController.java
+++ src/main/java/com/xjw/demo4/controller/HelloController.java
@@ -1,8 +1,15 @@
 package com.xjw.demo4.controller;
 
+import com.xjw.demo4.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -54,6 +61,49 @@ public class HelloController {
         log.info("name="+name+", age="+age);
         return "name="+name+", age="+age;
     }
+
+    /**
+     *  使用POST方法，在http的请求体中设置内容，并在控制台中打印出来
+     */
+    @PostMapping("/postString")
+    public String postString(HttpServletRequest request){
+        ServletInputStream sis = null;
+        StringBuilder sb = null;
+        try {
+            sis = request.getInputStream();
+            sb = new StringBuilder();
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = sis.read(buf)) != -1){
+                sb.append(new String(buf, 0, len));
+            }
+            System.out.println(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(sis != null){
+                    sis.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 使用@RequestBody注解，接受http请求体中的JSON数据，与entity类映射
+     */
+    @PostMapping("/user")
+    @ResponseBody   //有了ResponseBody，就可以返回String，Map类型等到前台
+    public Map<String, Object> getUser(@RequestBody User user){     //有了RequestBody，就可以接受前台传来的JSON类型数据
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("user", user);
+        log.info(map.toString());
+        return map;
+    }
+
 
 
 }
